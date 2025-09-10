12a9b9810f9e4d4ce3fdb70536cb4afef4b23a9c
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-09-10 08:55:37 -0400

    Finished Task 2 Began Task 3

---
+++ src/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        double springMaintenance;
+        double summerMaintenance;
+        double fallMaintenance;
+        double winterMaintenance;
+
+    }
+}
